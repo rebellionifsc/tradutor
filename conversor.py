51c3fbd2034797b235d68c09dc8d0ff6d8ef28df
Layza Cristini <layza.cristini@gmail.com>
2018-05-04 17:17:24 -0300

    Add files via upload

--- conversor.py
+++ conversor.py
@@ -1,11 +1,27 @@
 from flask import Flask, request
 import json
+import mysql.connector
 app = Flask(__name__)
 
 @app.route("/", methods = ['POST'])
 def pingpong():
     print (request.json)
     return json.dumps(request.json)
+
+@app.route("/busca", methods = ['POST'])
+def busca():
+    req = request.json
+    cnx = mysql.connector(user='root',
+                          password='buracoquente',
+                          database='Heyceitas',
+                          host='localhost')
+    cursor = cnx.cursor()
+    resultado = cursor.execute( 'select * from ingrediente where produto = (select id from produto where nome = "alho")')
+    print (resultado)
+    cursor.close()
+    cnx.close()
+    return resultado
+
 
 if __name__ ==  '__main__':
     app.run(host='0.0.0.0' ,
